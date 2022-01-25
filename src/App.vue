6cefa70940ca5f2a81ecfb7832c3a86dcaa024a8
YuwenLu <luyuwen2000@163.com>
2022-01-25 15:29:05 +0800

    进行了登陆页面设计，接下来需要调整样式

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,50 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/Login">Login</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <el-container>
+    <el-header>
+      <div id="app">
+        <div id="nav">
+          <router-link to="/">Home</router-link>
+          <router-link to="/about">About</router-link>
+          <router-link to="/Login">Login</router-link>
+        </div>
+        <router-view/>
+      </div>
+    </el-header>
+  </el-container>
+
+
 </template>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #409EFF;
+
 }
 
 #nav {
-  padding: 30px;
-  float: right;
+  width: 100%;
+  height: 40px;
+  margin-top: 0;
+  background-color: rgb(179, 216, 255);
 }
 
 #nav a {
+  background-color: aquamarine;
+  float: right;
+  width: 50px;
+  height: 40px;
+  margin-right: 10px;
   font-weight: bold;
   color: #2c3e50;
+
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #409EFF;
 }
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,110 @@
 <template>
-<div class="login">
-  <p>hello</p>
-  <div>
-    <el-button type="primary" :loading="true">加载中</el-button>
+
+<div id="body">
+  <div class="login-container">
+    <el-form :model="ruleForm2" :rules="rules2"
+             status-icon
+             ref="ruleForm2"
+             label-position="left"
+             label-width="0px"
+             class="demo-ruleForm login-page">
+      <h3 class="title">后疫情时代智慧物业管理系统</h3>
+      <h3 class="title">物业登陆</h3>
+      <el-form-item prop="username">
+        <el-input type="text"
+                  v-model="ruleForm2.username"
+                  auto-complete="off"
+                  placeholder="用户名"
+        ></el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input type="password"
+                  v-model="ruleForm2.password"
+                  auto-complete="off"
+                  placeholder="密码"
+        ></el-input>
+      </el-form-item>
+      <el-checkbox
+          v-model="checked"
+          class="rememberme"
+      >记住密码</el-checkbox>
+      <el-form-item style="width:100%;">
+        <el-button type="primary" style="width:100%;" @click="handleSubmit" :loading="logining">登录</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </div>
 </template>
 
 <script>
 export default {
-  name: "Login"
-}
+  data(){
+    return {
+      logining: false,
+      ruleForm2: {
+        username: 'admin',
+        password: '123456',
+      },
+      rules2: {
+        username: [{required: true, message: 'please enter your account', trigger: 'blur'}],
+        password: [{required: true, message: 'enter your password', trigger: 'blur'}]
+      },
+      checked: false
+    }
+  },
+  // methods: {
+  //   handleSubmit(event){
+  //     this.$refs.ruleForm2.validate((valid) => {
+  //       if(valid){
+  //         this.logining = true;
+  //         if(this.ruleForm2.username === 'admin' &&
+  //             this.ruleForm2.password === '123456'){
+  //           this.logining = false;
+  //           sessionStorage.setItem('user', this.ruleForm2.username);
+  //           this.$router.push({path: '/'});
+  //         }else{
+  //           this.logining = false;
+  //           this.$alert('username or password wrong!', 'info', {
+  //             confirmButtonText: 'ok'
+  //           })
+  //         }
+  //       }else{
+  //         console.log('error submit!');
+  //         return false;
+  //       }
+  //     })
+  //   }
+  // }
+};
 </script>
 
+<style scoped>
 
+#body{
+  background-image: url("../assets/loginPageBackground.jpeg") ;
+  background-repeat: repeat-x;
+  background-size: cover;
+  height: 800px;
+
+}
+
+.login-container {
+  width: 100%;
+  height: 100%;
+}
+.login-page {
+  -webkit-border-radius: 5px;
+  border-radius: 5px;
+  margin: 100px auto;
+  height: 400px;
+  width: 500px;
+  padding: 35px 35px 15px;
+  background: #fff;
+  border: 1px solid #eaeaea;
+  //box-shadow: 0 0 5px #cac6c6;
+}
+label.el-checkbox.rememberme {
+  margin: 0px 0px 15px;
+  text-align: left;
+}
+</style>
