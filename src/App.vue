61da3f779aa364636ecb0fd8fac0c7abd24ed2bc
Saltfish <Fish1103@foxmail.com>
2022-01-26 16:26:31 +0800

    添加总样式reset,分离header和main，添加main样式

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-container>
+  <el-container id="main-container">
     <el-header>
       <div class="buttonBox">
         <router-link to="/">
@@ -16,18 +16,32 @@
           <el-button type="primary" plain>登陆</el-button>
         </router-link>
       </div>
-      <router-view/>
     </el-header>
+    <el-main id="el-main-container">
+      <router-view/>
+    </el-main>
   </el-container>
 </template>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+}
 .el-header{
   text-align: center;
   line-height: 60px;
   background-color: rgb(160, 207, 255);
+  width: 100%;
 }
-
+#main-container{
+  width: 100%;
+  height: 100vh;
+}
+#el-main-container{
+  margin: 0;
+  padding: 0;
+}
 .buttonBox{
   float: right;
   width: 80px;
