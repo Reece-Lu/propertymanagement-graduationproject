aafc0b1ebcef2edbae501d2ea99eeb1f0f132096
YuwenLu <luyuwen2000@163.com>
2022-02-25 13:57:54 +0800

    实现模糊查询

--- src/components/defaultViews/Header.vue
+++ src/components/defaultViews/Header.vue
@@ -19,6 +19,7 @@ export default {
   name: "Header",
   methods:{
     logout(){
+      this.$router.push('/login')
       localStorage.removeItem('isLogin');
     }
   }
--- src/views/adminViews/ReportAndRepair.vue
+++ src/views/adminViews/ReportAndRepair.vue
@@ -1,45 +1,23 @@
 <template>
 <div class="reportandrepair">
+    <div style="width: 800px;float: left">
+      <el-input v-model="username" style="width: 500px" placeholder="请输入昵称"></el-input>
+      <el-button type="primary" style="width: 100px;margin-left: 50px" @click="load">搜索</el-button>
+    </div>
     <el-table
-        :data="tabledata"
+        :data="tableData"
         ref="multipleTable"
         tooltip-effect="dark"
         style="width: 100%"
        >
-      <el-table-column
-          type="selection"
-          width="55">
-      </el-table-column>
-      <el-table-column
-          prop="name"
-          label="姓名"
-          width="120">
-      </el-table-column>
-      <el-table-column
-          prop="age"
-          label="年龄"
-          width="120">
-      </el-table-column>
-      <el-table-column
-          prop="sex"
-          label="性别"
-          width="120">
-      </el-table-column>
-      <el-table-column
-          prop="department"
-          label="部门"
-          width="150">
-      </el-table-column>
-      <el-table-column
-          prop="floor"
-          label="楼层"
-          width="120">
-      </el-table-column>
-      <el-table-column
-          prop="area"
-          label="区域"
-          width="120">
-      </el-table-column>
+      <el-table-column type="selection" width="55"></el-table-column>
+      <el-table-column prop="id" label="ID" width="120"></el-table-column>
+      <el-table-column prop="username" label="用户名" width="120"></el-table-column>
+      <el-table-column prop="nickname" label="昵称" width="120"></el-table-column>
+      <el-table-column prop="email" label="邮箱" width="150"></el-table-column>
+      <el-table-column prop="phone" label="电话" width="120"></el-table-column>
+      <el-table-column prop="address" label="地址" width="120"></el-table-column>
+
       <el-table-column label="操作">
         <template >
           <el-button
@@ -55,17 +33,14 @@
       </el-table-column>
     </el-table>
     <div style="text-align: center;margin-top: 30px;">
-      <el-pagination
-          background
-          layout="prev, pager, next"
-          :total="total"
-          >
-      </el-pagination>
 
       <el-pagination
           background
-          :page-sizes="[5,10,15,20]"
-          :page-size="10"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page ="pageNum"
+          :page-sizes="[2,5,15,20]"
+          :page-size="pageSize"
           layout="total, sizes, prev, pager, next, jumper"
           :total="total"
       >
@@ -74,18 +49,43 @@
     </div>
 
 
-
 </div>
 </template>
 
 <script>
 export default {
   name: "ReportAndRepair",
+  data(){
+    return{
+      tableData:[],
+      total:0,
+      pageNum:1,
+      pageSize:5,
+      username:""
+    }
+  },
   created() {
     //请求分页查询数据
-    fetch("http://localhost:9090/user/page?pageNum=1&pageSize=2").then(res=>res.json()).then(res=>{
-      console.log(res)
-    })
+    this.load()
+  },
+  methods:{
+    handleSizeChange(pageSize){
+      console.log(pageSize)
+      this.pageSize=pageSize
+      this.load()
+    },
+    handleCurrentChange(pageNum){
+      console.log(pageNum)
+      this.pageNum=pageNum
+      this.load()
+    },
+    load(){
+      fetch("http://localhost:9090/user/page?pageNum="+this.pageNum+"&pageSize="+this.pageSize+"&username="+this.username).then(res=>res.json()).then(res=>{
+        console.log(res)
+        this.tableData=res.data
+        this.total=res.total
+      })
+    }
   }
 }
 </script>
