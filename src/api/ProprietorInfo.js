ab321446f1d6a0aa923d42605fc82cbada2118af
YuwenLu <luyuwen2000@163.com>
2022-03-06 13:25:29 +0800

    实现了业主修改个人信息功能

--- src/api/ProprietorInfo.js
+++ src/api/ProprietorInfo.js
@@ -4,3 +4,9 @@ import request from "@/utils/request";
 export function getProprietorInfo(data){
     return request.post('/pms/proprietorinfo/getproprietorinfo', data)
 }
+
+
+export function changeProprietorInfo(data){
+    return request.post('/pms/proprietorinfo/changeInfo', data)
+}
+
